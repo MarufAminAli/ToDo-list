59a0b18b61d7f8c32f41a6f1529e611fe6430219
Maruf Amin <marufamin549@gmail.com>
2023-02-21 22:36:43 +0500

    making logic for todo list

--- app.js
+++ app.js
@@ -1,53 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { urlencoded } = require('body-parser');
 const app = express();
+
+let items = [] ;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
-
 app.get ("/", (req, res) => {
-    
+
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
+   
+     let options ={
+        weekday: "long",
+        day :"numeric",
+        month:"long",
+     };
 
-   switch (currentDay) {
-    case 0:
-        day ="Sunday"
-        break;
-        case 1:
-         day ="Monday"
-        break;
-        case 2:
-        day ="Tesuday"
-        break;
-        case 3:
-        day ="Wednesday"
-        break;
-        case 4:
-        day ="Thursday"
-        break;
-        case 5:
-        day ="Friday"
-        break;
-        case 6:
-        day ="Saturday"
-        break;
-    default:
-        console.log("Error: current day is equal to: " + currentDay);
-    
-   }
-  
+    let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day})
+    res.render("list", {kindOfDay: day, newListItems: items})
 
 });
 
+app.post ( "/", (req, res) => { 
+   let item = req.body.newItem;
+   items.push(item)
+    res.redirect("/")
+ })
+
+
+ 
 app.listen (3000, function(){
 console.log("server is up and running");
 });
 
-
-
-/*
-*/
